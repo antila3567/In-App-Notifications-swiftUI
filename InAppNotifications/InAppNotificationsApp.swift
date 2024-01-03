cdb27b13b27d017112c1b93be727df3fd2877bc6
IvanL <antila3567@gmail.com>
2024-01-03 19:05:49 +0200

    Initial Commit

---
+++ InAppNotifications/InAppNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppNotificationsApp.swift
+//  InAppNotifications
+//
+//  Created by Иван Легенький on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct InAppNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
